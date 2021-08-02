b30be9cdf76111ff4af3ebd85a6160714cce6339
EduardoSantosF <eduardo.debest81@gmail.com>
2021-08-02 15:16:28 -0300

    Minor improvements

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 
 import Header from './components/Header';
-// import MainPage from './pages/MainPage';
+import MainPage from './Pages/MainPage';
 // import DetailsPage from './pages/DetailsPage';
 // import LoadingPage from './pages/LoadingPage';
 
 import './css/App.css';
+
+import { data } from './data';
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <MainPage products={data} />
       </div>
     );
   }
